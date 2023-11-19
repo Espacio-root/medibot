6130fa6cb73a9623d215773b7ca3ab75d9c0f687
Espacio-root <Abdullahd00124@gmail.com>
2023-11-19 22:47:06 +0530

    Final commit

--- local.py
+++ local.py
@@ -1,7 +1,7 @@
 # run locally
-# ~/projects/llama.cpp/main -m ~/projects/text-generation-webui/models/medllama2_7b_q4_0.gguf -n 256 --repeat_penalty 1.0 --color -i -r "User:" -f ~/projects/llama.cpp/prompts/template.txt
+# ~/projects/medibot-main/llama.cpp/main -m ~/projects/medibot-main/text-generation-webui/models/medllama2_7b_q4_0.gguf -n 256 --repeat_penalty 1.0 --color -i -r "User:" -f ~/projects/medibot-main/llama.cpp/prompts/template.txt
 
 # run text-generation-webui
-# ~/projects/text-generation-webui/start_linux.sh
+# ~/projects/medibot-main/text-generation-webui/start_linux.sh
 
 
